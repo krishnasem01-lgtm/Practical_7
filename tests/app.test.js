e94f1ea4caf4ade527858b9bcd2691ca222718b4
apurv070101 <apurvjiwaje2241835684@gmail.com>
2026-03-02 12:32:38 +0530

    Initial commit with Docker setup

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return success message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('CI/CD with Docker is Working 🚀');
+  });
+});
